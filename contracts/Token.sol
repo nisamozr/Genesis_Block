4acfe58edfe49e942edace0c44635b10c5232601
nisamozr <codeexplorkl10@gmail.com>
2022-01-04 20:40:26 +0530

    token contract complited

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,12 +2,57 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Token {
+  string public name = "Genesis  Token";
+  string public symbol = "GT";
   uint public totalSupply;
-
-  constructor(uint _initialSupply) public {
-    totalSupply = _initialSupply;
+  mapping (address => uint) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+  constructor(uint _initialSupply)  {
+      balanceOf[msg.sender] = _initialSupply;
+      totalSupply = _initialSupply;
 
   }
+
+
+  function transfer(address _to, uint _value) public returns(bool success){
+    require(balanceOf[msg.sender] >= _value);
+
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+
+    return true;
+    
+  }
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+       emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
 
 
 }
--- test/token.js
+++ test/token.js
@@ -5,17 +5,37 @@ const token = artifacts.require("Token.sol");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("token", function (/* accounts */) {
-  it("should assert true", async function () {
-    await token.deployed();
-    return assert.isTrue(true);
-  });
+contract("token", function (accounts ) {
+  // it("should assert true", async function () {
+  //   await token.deployed();
+  //   return assert.isTrue(true);
+  // });
   it("TotalSupply initialisation upon deployment", async ()=>{
     await token.deployed().then(instance=>{
       tokenInstance = instance;
       return tokenInstance.totalSupply();
     }).then(totalSupply=>{
       assert.equal(totalSupply.toNumber(),1000000, 'set totalsupply to 10000000')
+      return tokenInstance.balanceof(accounts[0]);
+    }).then(adminAccountBalance=>{
+      assert.equal(adminAccountBalance.toNumber(),1000000,"adiminBalace not ")
+    })
+  })
+
+  it("transfer token ",async()=>{
+   await token.deployed().then(instance=>{
+      tokenInstance = instance;
+      return tokenInstance.transfer.call(accounts[0],999999999999);
+    }).then(assert.fail).catch(error=>{
+      assert(error.message.indexOf('revert') >= 0, 'error massef contain rever massage')
+      return tokenInstance.Transfer.Call(accounts[1],250000, {from: accounts[0]})
+    }).then(recipt=>{
+      return tokenInstance.balanceof(accounts[1]);
+    }).then(balance=>{
+      assert.equal(balance.toNumber(),250000, "add the number to the resvie account")
+      return tokenInstance.balanceof(accounts[0]);
+    }).then(balance=>{
+      assert.equal(balance.toNumber(),75000000,"hhd")
     })
   })
 
